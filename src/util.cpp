23dc89015a3f517c1c64a5376e22fe045ac646d0
Lauren <laurayco@outlook.com>
2019-07-31 17:55:24 -0500

    toggle functionality based on debug flag

--- src/util.cpp
+++ src/util.cpp
@@ -1,7 +1,22 @@
 #include "util.h"
 
+#ifdef DEBUG
+
 void log_argv(char** argv) {
+    std::cout << "LOGGING ARGV" << std::endl;
     while(*argv) {
+        std::cout << "another entry: ";
         std::cout << *argv++ << std::endl;
     }
+    std::cout << "DONE LOGGING ARGV" << std::endl;
 }
+
+#else
+
+void log_argv(char** argv) {
+    std::cout << "LOGGING ARGV" << std::endl;
+    std::cout << "DONE LOGGING ARGV" << std::endl;
+    return;
+}
+
+#endif
