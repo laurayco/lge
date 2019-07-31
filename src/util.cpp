15eeb9ab12112ff5fb8c207cee5dd417324df3c6
Lauren <laurayco@outlook.com>
2019-07-31 18:34:07 -0500

    simplify util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -1,22 +1,12 @@
 #include "util.h"
 
-#ifdef DEBUG
-
 void log_argv(char** argv) {
-    std::cout << "LOGGING ARGV" << std::endl;
+#ifdef DEBUG
     while(*argv) {
         std::cout << "another entry: ";
         std::cout << *argv++ << std::endl;
     }
-    std::cout << "DONE LOGGING ARGV" << std::endl;
-}
-
 #else
-
-void log_argv(char** argv) {
-    std::cout << "LOGGING ARGV" << std::endl;
-    std::cout << "DONE LOGGING ARGV" << std::endl;
+#endif
     return;
 }
-
-#endif
